2a448183b6be15b121ff18c2bde9faa382306f27
ashishbajpaii <ashishbajpaee@gmail.com>
2025-03-22 16:29:46 +0530

    Updated frontend UI

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -11,7 +11,7 @@ function Course() {
   useEffect(()=>{
     const getBook= async()=>{
       try{
-      const res=await axios.get("http://localhost:4001/book");
+      const res=await axios.get("https://bookstore-backend-1-chms.onrender.com/book");
       console.log(res)
       setBook(res.data)
     }catch(error){
